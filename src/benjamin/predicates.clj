df89fb24b868875c2c41b72370569cd8f5716eb8
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2017-08-02 00:03:40 +0300

    predicates

---
+++ src/benjamin/predicates.clj
@@ -0,0 +1,13 @@
+(ns benjamin.predicates
+  (:require  [detijd.core :as time]))
+
+(def unique? #(some? %))
+(def today? #(if-let [date (first (vals %))]
+               (time/today? date)
+               false))
+(def last-3-days? #(if-let [date (first (vals %))]
+                     (time/last-days? date 3)
+                     false))
+(def last-3-months? #(if-let [date (first (vals %))]
+                       (time/last-months? date 3)
+                       false))

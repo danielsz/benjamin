1e80d796f24de7face1824aab1a5d0a2d3cab8e1
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2017-08-01 12:22:24 +0300

    logbook-fn

--- src/benjamin/core.clj
+++ src/benjamin/core.clj
@@ -2,30 +2,30 @@
 
 (def ^:dynamic persistence-fn nil)
 (def ^:dynamic success-fn nil)
+(def ^:dynamic logbook-fn nil)
 (def ^:dynamic events nil)
-(def ^:dynamic get-logbook nil)
 (def ^:dynamic allow-undeclared-events? nil)
 
 (def ^:dynamic config
   {:persistence-fn (fn [_ _] (throw (Exception.  "Please run 'set-config! :persistence-fn!` with a function of two arguments (entity and event)")))
    :success-fn (constantly true)
    :events #(throw (Exception. "Please set event and predicate map"))
-   :get-logbook :logbook
+   :logbook-fn :logbook
    :allow-undeclared-events? true})
 
 (defn set-config! [k v]
-  {:pre [(some #{k} [:persistence-fn :success-fn :events :get-logbook :allow-undeclared-events?])]}
+  {:pre [(some #{k} [:persistence-fn :success-fn :events :logbook-fn :allow-undeclared-events?])]}
   (alter-var-root #'config (fn [config] (assoc config k v))))
 
 (defn validate [entity event]
   (with-bindings {#'allow-undeclared-events? (:allow-undeclared-events? config)
-                  #'get-logbook (:get-logbook config)
+                  #'logbook-fn (:logbook-fn config)
                   #'events (:events config)}
     (when (fn? events) (events))
     (let [not-found (if allow-undeclared-events? (constantly false) (constantly true))
           pred (event events not-found)]
-      (if (get-logbook entity)
-        (not (some pred (filter event (get-logbook entity))))
+      (if (logbook-fn entity)
+        (not (some pred (filter event (logbook-fn entity))))
         true))))
 
 (defmacro with-logbook [entity event & body]
